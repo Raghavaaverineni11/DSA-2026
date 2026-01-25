b0753b5722be7fd6667484c53b1908fbb8e79cc4
Raghavaaverineni11 <raghavaoffc@gmail.com>
2026-01-24 19:39:45 -0600

    Initial commit with prefix sum project

---
+++ src/main/java/org/example/PrefixSum.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import java.util.*;
+public class PrefixSum {
+       public static void main(String[] args) {
+           int arr[] = {1, -20, -3, 30, 5, 4};
+           int target =7
+
+       }
+}
