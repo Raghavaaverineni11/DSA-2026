ede5949650882026bda392ae754c56a0d82e8f60
Raghavaaverineni11 <raghavaoffc@gmail.com>
2026-02-05 22:52:36 -0600

    Errors not rectified

--- src/main/java/org/example/PrefixSum.java
+++ src/main/java/org/example/PrefixSum.java
@@ -1,11 +1,19 @@
 package org.example;
 
 
-import java.util.*;
-public class PrefixSum {
-       public static void main(String[] args) {
-           int arr[] = {1, -20, -3, 30, 5, 4};
-           int target =7
+import java.util.HashMap;
+import java.util.Map;
 
-       }
+public Class PrefixSum {
+
+    int arr[]={1,2,3,4,5,6};
+    int target=5;
+    int runningSum=0;
+    Map<Integer,Integer> m=new HashMap<>();
+    m.put(0,0);
+    for(int i=0;i<arr.length;i++){
+
+
+
+
 }
---
+++ src/main/java/org/example/Twopointers.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Twopointers {
+
+    public static void main(String[] args){
+        int arr[]=new int[5];
+    }
+}
