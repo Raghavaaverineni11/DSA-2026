6ab4cdb71fdf0d738c8b4dbc48c437518355c357
Raghavaaverineni11 <raghavaoffc@gmail.com>
2026-02-14 11:53:56 -0600

    prefix sum pattern for product Array Except the Number Itself

--- src/main/java/org/example/PrefixSum.java
+++ src/main/java/org/example/PrefixSum.java
@@ -1,19 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/// Keepin in Mind Just multiply left subarray Eith right Subarray
+public class PrefixSum {
 
-import java.util.HashMap;
-import java.util.Map;
+    public static List<Integer> productArray(List<Integer> arr){
+        // 2 3 4 5
+        // res=1 2 6 24
+        List<Integer> res=new ArrayList<>();
+        res.add(1);
+        int prefix=1;
+        for(int i=1;i<arr.size();i++)
+        {
+            prefix=prefix*arr.get(i-1);                //  For prefixArray
+            res.add(prefix);
 
-public Class PrefixSum {
+        }
+         int suffix=1;
+        for(int i=arr.size()-1;i>=0;i--){
+            res.set(i,res.get(i)*suffix);              // Not creating suffix array just storing value of suffix array i suffix variable
+            suffix=suffix*arr.get(i);
+        }
 
-    int arr[]={1,2,3,4,5,6};
-    int target=5;
-    int runningSum=0;
-    Map<Integer,Integer> m=new HashMap<>();
-    m.put(0,0);
-    for(int i=0;i<arr.length;i++){
+        return res;
 
 
+    }
+
+    public static void main(String[] args){
+        List<Integer> a= Arrays.asList(2,3,4,5);
+        System.out.println(productArray(a));
+    }
 
 
 }
